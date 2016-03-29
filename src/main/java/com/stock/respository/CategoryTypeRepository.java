8a538c38228d09ec7ded744f88ca8c50bc524b10
Abdul Razak <razakcbe@gmail.com>
2016-03-30 00:29:59 +0530

    Added Transactional annotation for update method

--- src/main/java/com/stock/respository/CategoryTypeRepository.java
+++ src/main/java/com/stock/respository/CategoryTypeRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.stock.entity.CategoryType;
 import com.stock.entity.Product;
 
+@Transactional
 public interface CategoryTypeRepository extends JpaRepository<CategoryType, Integer>{
 	
 public List<CategoryType> findByProduct(Product productId);
--- src/main/java/com/stock/respository/ProductRepository.java
+++ src/main/java/com/stock/respository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.stock.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.stock.entity.Product;
 
+@Transactional
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
 	Product findByName(String name);
